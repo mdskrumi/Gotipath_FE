0e76a3919af6675d85110fa6d6492f0b3f31d19b
mdskrumi <mdskrumi@gmail.com>
2021-04-16 15:08:14 +0600

    options added

--- app/containers/LoginPage/index.js
+++ app/containers/LoginPage/index.js
@@ -4,11 +4,11 @@
  *
  */
 
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { Helmet } from 'react-helmet';
-import { FormattedMessage } from 'react-intl';
+
 import { createStructuredSelector } from 'reselect';
 import { compose } from 'redux';
 
@@ -17,19 +17,34 @@ import { useInjectReducer } from 'utils/injectReducer';
 import makeSelectLoginPage from './selectors';
 import reducer from './reducer';
 import saga from './saga';
-import messages from './messages';
+
+import DynamicSwitch from '../../components/DynamicSwitch/Loadable';
 
 export function LoginPage() {
   useInjectReducer({ key: 'loginPage', reducer });
   useInjectSaga({ key: 'loginPage', saga });
 
+  const options = ['Personal', 'Company'];
+  const [selectedOption, setSelectedOption] = useState(0);
+
   return (
     <div>
       <Helmet>
-        <title>LoginPage</title>
+        <title>Login | Gotipath</title>
         <meta name="description" content="Description of LoginPage" />
       </Helmet>
-      <FormattedMessage {...messages.header} />
+      <div className="login__page__body">
+        <div className="login__page__title">
+          <h1>Welcome to Gotipath</h1>
+        </div>
+        <div className="login__page__switch">
+          <DynamicSwitch
+            options={options}
+            setSelectedIndex={setSelectedOption}
+            selectedIndex={selectedOption}
+          />
+        </div>
+      </div>
     </div>
   );
 }
